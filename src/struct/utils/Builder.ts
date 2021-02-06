620434d10bc9f50703eb9003ec496bbedaca9d65
WaterBlade <zengjiangtao_yt@126.com>
2021-02-06 18:47:12 +0800

    change structure

--- src/struct/utils/Builder.ts
+++
@@ -1,3 +0,0 @@
-export abstract class Builder<T, U, P> {
-  constructor(protected struct: T, protected rebars: U, protected figures: P) {}
-}
--- src/struct/utils/index.ts
+++ src/struct/utils/index.ts
@@ -1,12 +1,6 @@
-export * from "./Builder";
 export * from "./Drawing";
 export * from "./FigureContent";
-export * from "./FigureBuilder";
-export * from "./FigureContainer";
 export * from "./PosGen";
-export * from "./PosRebar";
-export * from "./RebarBuilder";
-export * from "./RebarContainer";
 
 export * from "./Figure";
 export * from "./Rebar";
